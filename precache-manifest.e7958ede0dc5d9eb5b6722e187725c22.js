596daf8dea3dd8b686474272861fe478acfc2072
Facundo Rodriguez <69491651+facundon@users.noreply.github.com>
2021-02-09 16:07:43 -0300

    Add files via upload

---
+++ precache-manifest.e7958ede0dc5d9eb5b6722e187725c22.js
@@ -0,0 +1,301 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/app.65b289394e08716b5cf7.css"
+  },
+  {
+    "revision": "9922c0f69aeb8e9820a583f784453762",
+    "url": "/images/amex.svg"
+  },
+  {
+    "revision": "2edbf568491472c1f48ed97cd1abfacf",
+    "url": "/images/arrow-back.svg"
+  },
+  {
+    "revision": "24d174f4a5a1a34940852d713a66cec0",
+    "url": "/images/arrow.svg"
+  },
+  {
+    "revision": "e6e7c0e9aa4e804d38adc35637be55b9",
+    "url": "/images/background.svg"
+  },
+  {
+    "revision": "16b67a69b11e4adb99eede9a5e8eff46",
+    "url": "/images/breadcrumbs-arrow.svg"
+  },
+  {
+    "revision": "46e5dfb2cdad1754ee16cd521e551b7a",
+    "url": "/images/carousel-arrow.svg"
+  },
+  {
+    "revision": "2e279873fa0479e27bf8ff74f99eedd8",
+    "url": "/images/cart.svg"
+  },
+  {
+    "revision": "423a626c9c5659e6f50e9fc0fb8fff18",
+    "url": "/images/discover.svg"
+  },
+  {
+    "revision": "8c2f1b1a12ee68297a64eed295e0c2a0",
+    "url": "/images/favicons/android-chrome-144x144.png"
+  },
+  {
+    "revision": "9943d1f1264f28a7f30c83519745a135",
+    "url": "/images/favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "c05f1088f993a12a98559763173b123e",
+    "url": "/images/favicons/android-chrome-256x256.png"
+  },
+  {
+    "revision": "569f72d2eae0da2b8006066639969795",
+    "url": "/images/favicons/android-chrome-36x36.png"
+  },
+  {
+    "revision": "729f7dffdd9c44cffde33457c757b91d",
+    "url": "/images/favicons/android-chrome-384x384.png"
+  },
+  {
+    "revision": "0e23c44af766b8d3b86db1508a00f68a",
+    "url": "/images/favicons/android-chrome-48x48.png"
+  },
+  {
+    "revision": "7f07bc9db833c6322ed89ff51e866b38",
+    "url": "/images/favicons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "1a90fd5636a2afa0a1e975fc41157a6e",
+    "url": "/images/favicons/android-chrome-72x72.png"
+  },
+  {
+    "revision": "f949bf59f50ffb9863d7869a0ccc5dfc",
+    "url": "/images/favicons/android-chrome-96x96.png"
+  },
+  {
+    "revision": "b7da662cffebf7dbad660039fa4a41b7",
+    "url": "/images/favicons/apple-touch-icon-1024x1024.png"
+  },
+  {
+    "revision": "ee63382c90576a82bef942d485498334",
+    "url": "/images/favicons/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "d2e85a17676e1ecfd17b5bf20afb377d",
+    "url": "/images/favicons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "13ff978ede27ec9074dc385e57200ee8",
+    "url": "/images/favicons/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "c0d240f1be377fe96c6fe5afb283f8f0",
+    "url": "/images/favicons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "2840cf5a9ae9bcd327d24c2af0b07afe",
+    "url": "/images/favicons/apple-touch-icon-167x167.png"
+  },
+  {
+    "revision": "ff9387352dd308be4b7b34ac2840de21",
+    "url": "/images/favicons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "8318705c8d1d2a184857e8109e29ff29",
+    "url": "/images/favicons/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "725ccab0f781d2db409cdb042121c2aa",
+    "url": "/images/favicons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "42072e86329b04834ce66a3b38d4a787",
+    "url": "/images/favicons/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "cc14fd99c13d6de763a58814bc4bc173",
+    "url": "/images/favicons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "ff9387352dd308be4b7b34ac2840de21",
+    "url": "/images/favicons/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "ff9387352dd308be4b7b34ac2840de21",
+    "url": "/images/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "a5ba838c8e764fd6f796ec09958a75fa",
+    "url": "/images/favicons/apple-touch-startup-image-1182x2208.png"
+  },
+  {
+    "revision": "accc38e1b1114ec308bb465b965e8ed8",
+    "url": "/images/favicons/apple-touch-startup-image-1242x2148.png"
+  },
+  {
+    "revision": "2310c6c1573e9b7bd3da8963c0b090d2",
+    "url": "/images/favicons/apple-touch-startup-image-1496x2048.png"
+  },
+  {
+    "revision": "cb28b8f6906265f14f1b68b34759339b",
+    "url": "/images/favicons/apple-touch-startup-image-1536x2008.png"
+  },
+  {
+    "revision": "7e83c3170c5843111f1addb047dcde95",
+    "url": "/images/favicons/apple-touch-startup-image-320x460.png"
+  },
+  {
+    "revision": "893cc9ebce32ff91092f0de0299ba741",
+    "url": "/images/favicons/apple-touch-startup-image-640x1096.png"
+  },
+  {
+    "revision": "f512e7d06fe1175d2e506595ff3f1d55",
+    "url": "/images/favicons/apple-touch-startup-image-640x920.png"
+  },
+  {
+    "revision": "e4b180e2be310892fec2d6b3e0b3e2db",
+    "url": "/images/favicons/apple-touch-startup-image-748x1024.png"
+  },
+  {
+    "revision": "9497724db4d7386587d8bd1f788014d2",
+    "url": "/images/favicons/apple-touch-startup-image-750x1294.png"
+  },
+  {
+    "revision": "3dfeb5ce2c2afeb80cdb14c43537fffe",
+    "url": "/images/favicons/apple-touch-startup-image-768x1004.png"
+  },
+  {
+    "revision": "a4c4ae44d03d70fbddcb8a75c47dedd4",
+    "url": "/images/favicons/browserconfig.xml"
+  },
+  {
+    "revision": "a586b639ccf266f3c357d2fa244042ac",
+    "url": "/images/favicons/coast-228x228.png"
+  },
+  {
+    "revision": "29b3cf217d1496ab5a0bf5d6fc9e952c",
+    "url": "/images/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "15de96bd4a6d86959fae8e64f955ace5",
+    "url": "/images/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "f2230925aff9e23cf6f405e28e73696e",
+    "url": "/images/favicons/favicon.ico"
+  },
+  {
+    "revision": "2df221c0df87bc8b83296467b54355ec",
+    "url": "/images/favicons/firefox_app_128x128.png"
+  },
+  {
+    "revision": "242801edf4d9467b77732676d33746a2",
+    "url": "/images/favicons/firefox_app_512x512.png"
+  },
+  {
+    "revision": "45a73ea75d857a86727a3f5fcd82ee7d",
+    "url": "/images/favicons/firefox_app_60x60.png"
+  },
+  {
+    "revision": "b3bda8e67a8a3124bd232b28064cb06d",
+    "url": "/images/favicons/manifest.json"
+  },
+  {
+    "revision": "3bad13ea22cc3f7d03fa8e61f69bef89",
+    "url": "/images/favicons/manifest.webapp"
+  },
+  {
+    "revision": "8c2f1b1a12ee68297a64eed295e0c2a0",
+    "url": "/images/favicons/mstile-144x144.png"
+  },
+  {
+    "revision": "c3a500711787a4a0cf945f63f4023e59",
+    "url": "/images/favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "c5ed9308914461a63501e40e937b356e",
+    "url": "/images/favicons/mstile-310x150.png"
+  },
+  {
+    "revision": "02f2f206e9e29b4ac217fd5ae5b4c7e5",
+    "url": "/images/favicons/mstile-310x310.png"
+  },
+  {
+    "revision": "e1019036cd2d8ef1a890dd9da5ad341b",
+    "url": "/images/favicons/mstile-70x70.png"
+  },
+  {
+    "revision": "ea086856a6acf538ec35ce899dbae7a8",
+    "url": "/images/favicons/yandex-browser-50x50.png"
+  },
+  {
+    "revision": "74ca5bb0ff10841d62d3bfa09e6b41bc",
+    "url": "/images/favicons/yandex-browser-manifest.json"
+  },
+  {
+    "revision": "72f0b80556965d101b99735a5382c1b5",
+    "url": "/images/garbage.svg"
+  },
+  {
+    "revision": "280995e507a9a208edcfad3d1bdda1cf",
+    "url": "/images/instagram-icon.svg"
+  },
+  {
+    "revision": "35555a736d713af1d68af3f399b116f8",
+    "url": "/images/jcb.svg"
+  },
+  {
+    "revision": "f9b118a4c9879709171c4662358745d5",
+    "url": "/images/logo-small.svg"
+  },
+  {
+    "revision": "87a2d6f12e0e412ad39c32d63af1421d",
+    "url": "/images/logo.svg"
+  },
+  {
+    "revision": "9d17cdd532fb79575cc394a6aa698008",
+    "url": "/images/maestro.svg"
+  },
+  {
+    "revision": "50b1bd2483c8925bd359272d376440a8",
+    "url": "/images/mastercard.svg"
+  },
+  {
+    "revision": "114a0c361476162e7ab92a80efcf4038",
+    "url": "/images/no-photo.svg"
+  },
+  {
+    "revision": "794c5f9fc0af2947d89b369df1cf96b2",
+    "url": "/images/quote-icon.svg"
+  },
+  {
+    "revision": "15cc91d8fa045c71c7e65c5d72917410",
+    "url": "/images/search.svg"
+  },
+  {
+    "revision": "221eaa5b02938107b3ba74136a96aba0",
+    "url": "/images/sewing-machine.svg"
+  },
+  {
+    "revision": "2cbed1f3f96e36f656d71fd4d20b4322",
+    "url": "/images/subcategories.svg"
+  },
+  {
+    "revision": "5a93a28a584dc3157f803e6176b7181d",
+    "url": "/images/user.svg"
+  },
+  {
+    "revision": "eb63482e3c6153359a36eb5abc604bde",
+    "url": "/images/visa.svg"
+  },
+  {
+    "revision": "ebc70e6743b684542437cef59a35c39f",
+    "url": "/images/x.svg"
+  },
+  {
+    "revision": "8b6db406da1a2f4269de3a0cdf98fafe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2316b80715d3f69d28d",
+    "url": "/js/app.a269f000036b42c5d666.js"
+  }
+]);
